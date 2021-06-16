b6c5091f5d3dc1cc6f9b9f63d7c8ee6c656bf138
Wali-Siddiqui <33173811+Wali-Siddiqui@users.noreply.github.com>
2021-06-16 14:07:26 +0500

    Update welcome.sh

--- welcome.sh
+++ welcome.sh
@@ -3,8 +3,10 @@
 RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
-echo -n "----------------------------------------------------------------------------------"
-echo -e  "${GREEN}"; hostname -I; echo -e "${ENDCOLOR}"
+PublicIP='dig +short myip.opendns.com @resolver1.opendns.com'
+echo "----------------------------------------------------------------------------------"
+echo -e -n "${GREEN} \t Private IP : ";  hostname -I; echo -e "${ENDCOLOR}";
+echo -e -n "${GREEN} \t\t\tPublic IP : "; ${PublicIP}; echo -e "${ENDCOLOR}";
 echo -n "----------------------------------------------------------------------------------"
 echo -e "${GREEN}"; free -h -t --giga; echo -e "${ENDCOLOR}"
 echo -n "----------------------------------------------------------------------------------"
@@ -12,5 +14,3 @@ echo -e "${GREEN}"; df -hT /dev/sdb2; echo -e "${ENDCOLOR}"
 echo "----------------------------------------------------------------------------------"
 echo -e "${GREEN}"; df -hT /dev/sda1; echo -e "${ENDCOLOR}"
 echo "----------------------------------------------------------------------------------"
-
-
